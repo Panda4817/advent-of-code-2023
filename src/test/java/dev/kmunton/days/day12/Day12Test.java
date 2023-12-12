4854937381048beb4886df656e5587ca6ce5e7b2
Kanta Munton <kanta.munton@gmail.com>
2023-12-12 21:26:15 +0000

    Complete day 12

--- src/test/java/dev/kmunton/days/day12/Day12Test.java
+++ src/test/java/dev/kmunton/days/day12/Day12Test.java
@@ -10,6 +10,12 @@ class Day12Test {
 
     private static Day12 day;
     private static final List<String> INPUT = """
+        ???.### 1,1,3
+        .??..??...?##. 1,1,3
+        ?#?#?#?#?#?#?#? 1,3,1,6
+        ????.#...#... 4,1,1
+        ????.######..#####. 1,6,5
+        ?###???????? 3,2,1
         """.lines().toList();
     @BeforeAll
     static void beforeAll() {
@@ -18,12 +24,12 @@ class Day12Test {
 
     @Test
     void part1() {
-        assertEquals( 0, day.part1());
+        assertEquals( 21, day.part1());
 
     }
 
     @Test
     void part2() {
-        assertEquals( 0, day.part2());
+        assertEquals( 525152, day.part2());
     }
 }
