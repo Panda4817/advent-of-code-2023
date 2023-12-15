cf2509031c37f14827e1635dee4420caf0f63bb8
Kanta Munton <kanta.munton@gmail.com>
2023-12-15 08:46:50 +0000

    Complete day 15

---
+++ src/main/java/dev/kmunton/days/day15/Lens.java
@@ -0,0 +1,31 @@
+package dev.kmunton.days.day15;
+
+public class Lens {
+    private final String label;
+    private long focalLength;
+
+    public Lens(String label, long focalLength) {
+        this.label = label;
+        this.focalLength = focalLength;
+    }
+
+    public long focalLength() {
+        return focalLength;
+    }
+
+    public String label() {
+        return label;
+    }
+
+    public void setFocalLength(long focalLength) {
+        this.focalLength = focalLength;
+    }
+
+    @Override
+    public String toString() {
+        return "Lens{" +
+            "label='" + label + '\'' +
+            ", focalLength=" + focalLength +
+            '}';
+    }
+}
