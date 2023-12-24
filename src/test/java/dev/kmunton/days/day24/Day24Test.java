e0d5b64b986576854d317ae7e282bdcfc82fa569
Kanta Munton <kanta.munton@gmail.com>
2023-12-24 14:56:58 +0000

    Complete day 24

--- src/test/java/dev/kmunton/days/day24/Day24Test.java
+++ src/test/java/dev/kmunton/days/day24/Day24Test.java
@@ -10,6 +10,11 @@ class Day24Test {
 
     private static Day24 day;
     private static final List<String> INPUT = """
+        19, 13, 30 @ -2,  1, -2
+        18, 19, 22 @ -1, -1, -2
+        20, 25, 34 @ -2, -2, -4
+        12, 31, 28 @ -1, -2, -1
+        20, 19, 15 @  1, -5, -3
         """.lines().toList();
     @BeforeAll
     static void beforeAll() {
@@ -18,12 +23,12 @@ class Day24Test {
 
     @Test
     void part1() {
-        assertEquals( 0, day.part1());
+        assertEquals( 2, day.findIntersectionInTestArea(7, 27));
 
     }
 
     @Test
     void part2() {
-        assertEquals( 0, day.part2());
+        assertEquals( 47, day.part2());
     }
 }
