3740b814da9be839645e694ca7eeb5610f224ac7
Kanta Munton <kanta.munton@gmail.com>
2023-12-25 17:23:19 +0000

    Complete day 25

--- src/main/java/dev/kmunton/days/day25/Day25.java
+++ src/main/java/dev/kmunton/days/day25/Day25.java
@@ -1,19 +1,63 @@
 package dev.kmunton.days.day25;
 
 import dev.kmunton.days.Day;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import org.jgrapht.Graph;
+import org.jgrapht.alg.StoerWagnerMinimumCut;
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.graph.Multigraph;
 
 public class Day25 implements Day {
+
+    private final Map<String, Set<String>> connections = new HashMap<>();
+    private final Set<String> components = new HashSet<>();
 
     public Day25(List<String> input) {
         processData(input);
     }
 
     public void processData(List<String> input) {
+        for (var line : input) {
+            var parts = line.split(": ");
+            var key = parts[0];
+            var values = parts[1].split(" ");
+            if (connections.containsKey(key)) {
+                connections.get(key).addAll(Arrays.asList(values));
+            } else {
+                connections.put(key, new HashSet<>(Arrays.asList(values)));
+            }
+            for (var value : values) {
+                if (connections.containsKey(value)) {
+                    connections.get(value).add(key);
+                } else {
+                    connections.put(value, new HashSet<>(Collections.singletonList(key)));
+                }
+            }
+            components.add(key);
+            components.addAll(Arrays.stream(values).toList());
+        }
     }
 
     public long part1() {
-        return 0;
+        Graph<String, DefaultEdge> stringGraph = new Multigraph<>(DefaultEdge.class);
+        for (var s: components) {
+            stringGraph.addVertex(s);
+        }
+        for (var key : connections.keySet()) {
+            for (var value : connections.get(key)) {
+                stringGraph.addEdge(key, value);
+            }
+        }
+
+        var s = new StoerWagnerMinimumCut<>(stringGraph);
+        var v = s.minCut();
+        return (long) v.size() * (components.size() - v.size());
 
     }
 
--- src/test/java/dev/kmunton/days/day25/Day25Test.java
+++ src/test/java/dev/kmunton/days/day25/Day25Test.java
@@ -10,6 +10,19 @@ class Day25Test {
 
     private static Day25 day;
     private static final List<String> INPUT = """
+        jqt: rhn xhk nvd
+        rsh: frs pzl lsr
+        xhk: hfx
+        cmg: qnr nvd lhk bvb
+        rhn: xhk bvb hfx
+        bvb: xhk hfx
+        pzl: lsr hfx nvd
+        qnr: nvd
+        ntq: jqt hfx bvb xhk
+        nvd: lhk
+        lsr: lhk
+        rzs: qnr cmg lsr rsh
+        frs: qnr lhk lsr
         """.lines().toList();
     @BeforeAll
     static void beforeAll() {
@@ -18,7 +31,7 @@ class Day25Test {
 
     @Test
     void part1() {
-        assertEquals( 0, day.part1());
+        assertEquals( 54, day.part1());
 
     }
 
